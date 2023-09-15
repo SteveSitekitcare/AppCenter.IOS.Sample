95fc7e1995fafff3b3fa450d2e17d62b27418e1b
Steve Parry <steven.parry@sitekitcare.co.uk>
2023-09-15 20:54:32 +0100

    sample

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            AppCenter.Start("ios=<insert secret>",
+                  typeof(Analytics), typeof(Crashes));
+        }
+    }   
+}
